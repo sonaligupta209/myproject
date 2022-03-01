fda8fb092e96bd24b97f17d668659d823843fcb6
Sonali GitHub <gsona209@gamil.com>
2022-03-01 23:29:06 +0530

    jdbcdemo2

--- jdbcdemo/src/jdbcdemo/JDBCDemo.java
+++ jdbcdemo/src/jdbcdemo/JDBCDemo.java
@@ -2,19 +2,22 @@ package jdbcdemo;
 import java.sql.*;
 public class JDBCDemo
 {
-public static void main(String[] args) {
-	try{
-		Class.forName("com.mysql.jdbc.driver");
+public static void main(String[] args)throws Exception {
+	
+		Class.forName("com.mysql.cj.jdbc.Driver");
 
-	    Connection con=DriverManager.getConnection("jdbc:mysql://localhost::3306/college","root","sonali@123");
+	    Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/college","root","Sonali@123");
 	    Statement st =con.createStatement();
 	    System.out.println("inserting records");
-	    String sql ="Insert into college values(53,'sonu')";
-	    st.executeUpdate(sql);	
-       }
-	catch(Exception e) {
-		
-		 System.out.println(e);
-}
+	    //String sql ="CREATE TABLE CUSTOMER(ID INT,NAME VARCHAR(20))";
+	   // String sql1 ="INSERT INTO CUSTOMER(ID,NAME)VALUES (1, 'Ramesh' );";
+	      //String sql2 ="INSERT INTO CUSTOMER(ID,NAME)VALUES (2, 'sona' );";
+	      String sql3 ="DELETE  FROM CUSTOMER WHERE ID=1";  
+	    st.executeUpdate(sql3);	
+       
+	
+
+	st.close();
+	con.close();
 }
 }
