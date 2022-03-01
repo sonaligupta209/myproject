53336cd5043284be6193aa1f1e67cafb21e655e9
Sonali GitHub <gsona209@gamil.com>
2022-03-01 11:09:52 +0530

    equal()

---
+++ equal/src/equal/EqualsDemo2.java
@@ -0,0 +1,15 @@
+package equal;
+
+public class EqualsDemo2 {
+
+	public static void main(String[] args) {
+        String s1 = new String("HELLO");
+        String s2 = new String("HELLO");        
+         
+        System.out.println("s1 and s2 are equal : "+(s1==s2));      
+        System.out.println("s1 and s2 are equal : "+(s1.equals(s2)));  
+ 
+        s1 = s2;
+        System.out.println("s1 and s2 are equal : "+(s1==s2));         
+    }
+}
