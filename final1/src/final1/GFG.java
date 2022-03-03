d6813514d952c9febca070e007afab2e535824f4
Sonali GitHub <gsona209@gamil.com>
2022-03-03 10:03:00 +0530

    finalkeyword

---
+++ final1/src/final1/GFG.java
@@ -0,0 +1,36 @@
+package final1;
+
+
+class GFG {
+
+
+	final int THRESHOLD = 5;
+	
+	
+	final int CAPACITY;
+	
+	
+	final int MINIMUM;
+
+	static final double PI = 3.141592653589793;
+	
+	static final double EULERCONSTANT;
+	
+
+	{
+		CAPACITY = 25;
+	}
+	
+	
+	static{
+		EULERCONSTANT = 2.3;
+	}
+	
+
+	public GFG()
+	{
+		MINIMUM = -1;
+	}
+		
+}
+
