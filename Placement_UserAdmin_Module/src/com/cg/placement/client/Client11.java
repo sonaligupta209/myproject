49fd7723d8d5505cf6d808bceed06ddaeec640d1
Sonali GitHub <gsona209@gamil.com>
2022-04-19 17:15:22 +0530

    project_modules

--- Placement_UserAdmin_Module/src/com/cg/placement/client/Client11.java
+++ Placement_UserAdmin_Module/src/com/cg/placement/client/Client11.java
@@ -11,7 +11,8 @@ import com.cg.placement.service.IUserService;
 import com.cg.placement.service.UserServiceImpl;
 public class Client11 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		
 		
 	
